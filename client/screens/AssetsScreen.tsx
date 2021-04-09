37bc3a937ce43e1a32b7e4e80091c12c9fb83932
Darryl Yeo <dev@darryl-yeo.com>
2021-04-09 02:26:52 +0000

    Create multiple asset sections (attempt to use SectionList)

--- client/screens/AssetsScreen.tsx
+++ client/screens/AssetsScreen.tsx
@@ -2,57 +2,119 @@ import * as React from 'react'
 
 import { layout, text } from '../styles/styles'
 
-import { FlatList, ScrollView, TouchableOpacity, View } from 'react-native'
-import { Card, Container, ListSeparator, Separator, Spacer, Text, Image } from '../components/ThemedComponents'
+import { FlatList, SectionList, ScrollView, TouchableOpacity, View } from 'react-native'
+import { Card, Container, ListSeparator, Separator, SectionSpacer, Spacer, Text, Image } from '../components/ThemedComponents'
 
-const baseAssets = [
+const sections = [
 	{
-		name: 'Celo Gold',
-		symbol: 'CELO',
-		image: require('../assets/images/celo-icon.png')
+		title: 'Celo Native Assets',
+		data: [
+			{
+				name: 'Celo Gold',
+				symbol: 'CELO',
+				image: require('../assets/images/celo-icon.png')
+			},
+			{
+				name: 'Celo Dollar',
+				symbol: 'cUSD',
+				image: require('../assets/images/cusd-icon.png')
+			},
+			{
+				name: 'Celo Euro',
+				symbol: 'cEUR',
+				image: require('../assets/images/ceur-icon.png')
+			}
+		]
 	},
 	{
-		name: 'Celo Dollar',
-		symbol: 'cUSD',
-		image: require('../assets/images/cusd-icon.png')
-	},
-	{
-		name: 'Celo Euro',
-		symbol: 'cEUR',
-		image: require('../assets/images/ceur-icon.png')
-	},
+		title: 'Other Assets',
+		data: [
+			{
+				name: 'Moola cUSD',
+				symbol: 'mcUSD',
+				image: require('../assets/images/cusd-icon.png')
+			},
+			{
+				name: 'Moola cEUR',
+				symbol: 'mcEUR',
+				image: require('../assets/images/ceur-icon.png')
+			},
+			{
+				name: 'Moss CO2',
+				symbol: 'cMCO2',
+				image: require('../assets/images/celo-icon.png')
+			}
+		]
+	}
 ]
+
+const baseAssets = sections[0].data
 
 export default function AssetsScreen() {
 	return (
 		<ScrollView>
-			<Container>
-				{/* <Text style={text.h2}>Wallets</Text>
-				<Separator /> */}
-				<Text style={text.h2}>Celo Native Assets</Text>
-				<Spacer />
-				<Card>
-					<FlatList
-						ItemSeparatorComponent={ListSeparator}
-						data={baseAssets}
-						style={layout.cardList}
-						renderItem={({ item, index, separators }) => (
-							<TouchableOpacity>
-								<View key="symbol" style={[layout.cardListItem, layout.row]}>
-									<Image source={item.image} style={{width: 48, height: 48}} />
-									<Spacer />
-									<View style={[layout.column, {flex: 1}]}>
-										<Text style={text.h3}>{item.name}</Text>
-										<Text style={text.caption}>{item.symbol}</Text>
-									</View>
-									<Spacer />
-									<Text style={text.strong}>$$$</Text>
-								</View>
-							</TouchableOpacity>
-						)}
-					/>
-				</Card>
-			</Container>
+			{/* <Text style={text.h2}>Wallets</Text>
+			<Separator /> */}
+			<FlatList
+				style={layout.container}
+				data={sections}
+				renderItem={({ item: { title, data } }) => (
+					<>
+						<Text style={text.h2}>{title}</Text>
+						<Spacer />
+						<Card style={layout.shadowed}>
+							<FlatList
+								data={data}
+								style={layout.cardList}
+								renderItem={({ item, index, separators }) => (
+									<TouchableOpacity>
+										<View key="symbol" style={[layout.cardListItem, layout.row]}>
+											<Image source={item.image} style={{width: 48, height: 48}} />
+											<Spacer />
+											<View style={[layout.column, {flex: 1}]}>
+												<Text style={text.h3}>{item.name}</Text>
+												<Text style={text.caption}>{item.symbol}</Text>
+											</View>
+											<Spacer />
+											<Text style={text.strong}>$$$</Text>
+										</View>
+									</TouchableOpacity>
+								)}
+								ItemSeparatorComponent={ListSeparator}
+							/>
+						</Card>
+					</>
+				)}
+				ItemSeparatorComponent={SectionSpacer}
+			/>
 		</ScrollView>
 	)
+
+	// return (
+	// 	<SectionList
+	// 		sections={sections}
+	// 		keyExtractor={(item, index) => item.symbol}
+
+	// 		renderSectionHeader={({ section: { title } }) => (
+	// 			<Text style={text.h2}>{title}</Text>
+	// 		)}
+	// 		renderItem={({ item, index, separators }) => (
+	// 			<TouchableOpacity>
+	// 				<View style={[layout.cardListItem, layout.row]}>
+	// 					<Image source={item.image} style={{width: 48, height: 48}} />
+	// 					<Spacer />
+	// 					<View style={[layout.column, {flex: 1}]}>
+	// 						<Text style={text.h3}>{item.name}</Text>
+	// 						<Text style={text.caption}>{item.symbol}</Text>
+	// 					</View>
+	// 					<Spacer />
+	// 					<Text style={text.strong}>$$$</Text>
+	// 				</View>
+	// 			</TouchableOpacity>
+	// 		)}
+
+	// 		ItemSeparatorComponent={ListSeparator}
+	// 		SectionSeparatorComponent={Separator}
+	// 	/>
+	// )
 }
