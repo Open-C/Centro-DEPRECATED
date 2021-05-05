bf8bbcd4f66219101ccf30b2bb8120392d7a802d
Dylan Mooers <dmooers@calpoly.edu>
2021-05-04 17:39:21 -0700

    Updated Contract Caller superclass to have a method to approve tokens before sending them, so that ERC20s can be sent easier

--- contracts/contracts/ContractCaller.sol
+++ contracts/contracts/ContractCaller.sol
@@ -1,4 +1,6 @@
 pragma solidity >0.5.0;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 
 contract ContractCaller {
 	function delegate(address _target, bytes memory _calldata) internal returns (bytes memory response) {
@@ -12,34 +14,23 @@ contract ContractCaller {
 		}
 
 		return returnData;
-		// assembly{
-		// 	let succeeded := delegatecall(gas(), _target, add(_calldata, 0x20), mload(_calldata), 0, 0)
-		// 	let size := returndatasize()
-
-		// 	response := mload(0x40)
-		// 	mstore(0x40, add(response, and(add(add(size, 0x20), 0x1f), not(0x1f))))
-		// 	mstore(response, size)
-		// 	returndatacopy(add(response, 0x20), 0, size)
-
-		// 	switch iszero(succeeded)
-		// 		case 1 {
-		// 			// throw if delegatecall failed
-		// 			returndatacopy(0x00, 0x00, size)
-		// 			revert(0x00, size)
-		// 		}
-		// }
 	}
 
-	function sendCelo(address payable _target, uint256 _value, bytes memory _calldata) internal returns (bytes memory response) {
+	function callContract(address payable _target, uint256 _value, bytes memory _calldata) internal returns (bytes memory response) {
 		require(_target != address(0), "Target invalid!");
-		(bool success, bytes memory returnData) = _target.call.value(_value)(_calldata);
+		(bool success, bytes memory returnData) = _target.call{value: _value}(_calldata);
 
 		assembly {
 			if eq(success, 0) {
 				revert(add(returnData, 32), returndatasize())
 			}
 		}
-
 		return returnData;
+	}
+
+	function approveToken(address _token, address payable _toApprove, uint256 _amt) internal {
+		require(_token != address(0), "Invalid token address");
+		require(_toApprove != address(0), "Invalid approvee");
+		IERC20(_token).approve(_toApprove, _amt);
 	}
 }
