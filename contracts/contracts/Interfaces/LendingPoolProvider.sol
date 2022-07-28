caa041d3118ead5b245d318f75a4d52b0d85ba20
Dylan Mooers <dylmooers@gmail.com>
2022-07-27 21:31:48 -0700

    last commit

--- contracts/contracts/Interfaces/LendingPoolProvider.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: ISC
-
-pragma solidity >0.5.0;
-
-abstract contract ILendingPoolAddressesProvider {
-    function getLendingPool() public view virtual returns (address);
-    function getLendingPoolCore() public view virtual returns (address payable);
-}
